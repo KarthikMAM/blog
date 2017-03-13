515843c5619a2f6beb29eb088db874abe9bd8e5c
Karthik M A M <karthik_m_a_m@outlook.com>
2017-03-14 00:09:03 +0530

    basic api call

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import request from 'superagent';
 
-export function HelloWorld({name}) {
-  return <h1> Hello {name} </h1>;
+export class HelloWorld extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      err: null
+    };
+  }
+
+  static propTypes = {
+    name: React.PropTypes.string
+  };
+
+  componentDidMount() {
+    new Promise((resolve, reject) => {
+      request
+        .get('http://localhost:3000/about.json')
+        .end((err, res) => {
+          if (err || !res.ok) { reject(err); return; }
+
+          res = JSON.parse(res.text);
+
+          res.success ? resolve(res.payload) : reject(err);
+        });
+    }).then(
+      res => this.setState({ name: res.name, err: null }),
+      err => this.setState({ err })
+      );
+  }
+
+  render() {
+    return this.state.err === null ?
+      <h1> Hello {this.state.name} </h1> :
+      <h1> {JSON.stringify(this.state.err)} </h1>;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import {HelloWorld} from './HelloWorld';
 
 ReactDOM.render(
-  <HelloWorld name="Karthik M A M" />,
+  <HelloWorld />,
   document.getElementById('root')
 );
